235137262cf99cb33758c353d868c9688f6348c2
HectorIlarraza <53978521+HectorIlarraza@users.noreply.github.com>
2022-05-18 02:50:30 -0500

    Fixed db schema and seed errors

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,27 +1,27 @@
 DROP DATABASE IF EXISTS inventory_dev;
 CREATE DATABASE inventory_dev;
 
-\C inventory_dev;
+\c inventory_dev;
 
 CREATE TABLE inventory (
     product_id SERIAL PRIMARY KEY,
-    store_id SERIAL PRIMARY KEY,
+    store_id INTEGER UNIQUE NOT NULL,
     stock_available BOOLEAN
 );
 
 CREATE TABLE product (
     id SERIAL PRIMARY KEY,
-    serial_num SERIAL PRIMARY KEY,
+    serial_num VARCHAR NOT NULL,
     name TEXT NOT NULL,
     price INTEGER,
     description TEXT NOT NULL,
     weight INTEGER,
     product_id INTEGER REFERENCES inventory (product_id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE store (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     address TEXT NOT NULL,
     store_id INTEGER REFERENCES inventory (store_id) ON DELETE CASCADE
-)
+);
--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,18 +1,18 @@
 \c inventory_dev;
 
-INSERT INTO inventory (stock_available) VALUES
-(true),
-(true),
-(false),
-(false),
-(true);
+INSERT INTO inventory (store_id, stock_available) VALUES
+('1',true),
+('2',true),
+('3',false),
+('4',false),
+('5',true);
 
-INSERT INTO product (product_id, name, price, description, weight) VALUES
-('1', 'Heated Jacket', 200, 'A heated jacket  with tailored design and breathable fabric. Water and wind resistant exterior ensures you keep warm and dry in unpredicted weather conditions.', 11),
-('2', 'Fresh Beef Brisket Special Trimmed', 50, 'HEB beef brisket has a superior level of fat marbling and flavor. Brisket can be slow cooked, barbecued, oven roasted, and more!', 9),
-('4', 'Henckels Dynamic Knife Block Set', 136, 'Henckels makes essential kitchen knives that home chefs reach for everyday!', 14),
-('3', 'SAMSUNG 75" Class The Terrace Outdoor QLED 4K Smart TV with HDR', 5345, 'With The Terrace, gather around an outdoor TV designed to look incredibly vivid and bright, even in daylight.', 130),
-('5', 'MLB The Show 21 Standard Edition - PlayStation 4', 8, 'This is the all new MLB® The Show™', 3);
+INSERT INTO product (product_id, serial_num, name, price, description, weight) VALUES
+('1', '21341235', 'Heated Jacket', 200, 'A heated jacket  with tailored design and breathable fabric. Water and wind resistant exterior ensures you keep warm and dry in unpredicted weather conditions.', 11),
+('2', '12512123', 'Fresh Beef Brisket Special Trimmed', 50, 'HEB beef brisket has a superior level of fat marbling and flavor. Brisket can be slow cooked, barbecued, oven roasted, and more!', 9),
+('4', '12353125', 'Henckels Dynamic Knife Block Set', 136, 'Henckels makes essential kitchen knives that home chefs reach for everyday!', 14),
+('3', '31252313', 'SAMSUNG 75" Class The Terrace Outdoor QLED 4K Smart TV with HDR', 5345, 'With The Terrace, gather around an outdoor TV designed to look incredibly vivid and bright, even in daylight.', 130),
+('5', '12353125', 'MLB The Show 21 Standard Edition - PlayStation 4', 8, 'This is the all new MLB® The Show™', 3);
 
 INSERT INTO store (store_id, name, address) VALUES
 ('5', 'Best Buy', '19000 Limestone Commercial Dr, Pflugerville, TX 78660'),
